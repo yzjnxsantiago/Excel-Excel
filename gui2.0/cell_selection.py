276eb016c7d92f257f0b49c5b938886534a95dfd
Santiago <santiago.s.sira@gmail.com>
2023-04-04 16:29:47 -0700

    Updated GUI

--- gui2.0/cell_selection.py
+++ gui2.0/cell_selection.py
@@ -42,15 +42,4 @@ def cell_selection(root: Frame):
                 background= "#800020", foreground='White', activebackground="#a6022b" , activeforeground="White", cursor="hand2", state="disabled"
                 )
     new_cells_btn.grid(row=1, rowspan= 2, column=21, padx=4)
-<<<<<<< HEAD
-
     
-    
-
-#Let me describe my project, let me know how I can organize this project, how I should divide the apps,  and if you forsee any potential issues:
-
-#My website will first be a homepage. This homepage will showcase the legends ascension arena, the subscription plans, the elo system and account registration. This will be the first area the user will be able to access to find information about how legends ascension arena. Next once the user logs in they will have a dashboard. This dashboard will have a profile that will showcase the league of legends account (stuff like rank, level and profile name), it will also show the statistics of their matches with legends ascension arena, the teams they have been and the current team they are on as well as their schedule. Some of these things will have separate pages where the user can find even more detailed information. For example, they can click on one of the teams they were on and find out their match history. Next we will need another page or section for the user to sign up for a league. Here they will be able to sign up for a league depending on their rank, how many teams they want to play with, and how much money they want to compete for. When they sign up, this will then be tossed to the backend. The backend will need to determine which teams will need to play with each other and the schedule of the game. These games will then need to be automatically created with a tournament passcode. The game will automatically close 5 minutes after the time. After the game is completed, the teams will need to 
-=======
-    
-    
->>>>>>> 4586a4b (Updated GUI,)
